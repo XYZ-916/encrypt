1fdddcf4e4c4ad89cee6e65cab1ce15043f517ce
liyang <liyang@leocool.com>
2017-04-10 18:08:43 +0800

    update md5

--- md5/md5.h
+++ md5/md5.h
@@ -1,30 +1,52 @@
-#ifndef __ENCRYPT_MD_FIVE__
-#define __ENCRYPY_MD_FIVE__
+// md5.h
 
-#include <iostream>
+#ifndef _MD5_H
+#define _MD5_H
+
 #include <string>
+//#include <iostream>
 
-/* Type define */
-typedef unsigned char byte;
-typedef unsigned long ulong;
-
-
-using std::string;
-using std::ifstream;
-
-class MD5 {
+class MD5
+{
 public:
+	typedef unsigned int size_type; // must be 32bit
+
 	MD5();
-	~MD5();
+	MD5(std::string text);
+	void update(const unsigned char *buf, size_type length);
+	void update(const char *buf, size_type length);
+	MD5& finalize();
+	std::string hexdigest() const;
+	//friend std::ostream& operator<<(std::ostream&, MD5 md5);
 
-	void encryptString(const string &str);
-	void encryptFile(const string &fileName);
+private:
+	void init();
+	typedef unsigned char uint1; //  8bit
+	typedef unsigned int uint4;  // 32bit
+	enum { blocksize = 64 }; // VC6 won't eat a const static int here
 
-protected:
-	void encrypt(const byte *input, ulong length);
+	void transform(const uint1 block[blocksize]);
+	static void decode(uint4 output[], const uint1 input[], size_type len);
+	static void encode(uint1 output[], const uint4 input[], size_type len);
 
+	bool finalized;
+	uint1 buffer[blocksize]; // bytes that didn't fit in last 64 byte chunk
+	uint4 count[2];   // 64bit counter for number of bits (lo, hi)
+	uint4 state[4];   // digest so far
+	uint1 digest[16]; // the result
 
+	// low level logic operations
+	static inline uint4 F(uint4 x, uint4 y, uint4 z);
+	static inline uint4 G(uint4 x, uint4 y, uint4 z);
+	static inline uint4 H(uint4 x, uint4 y, uint4 z);
+	static inline uint4 I(uint4 x, uint4 y, uint4 z);
+	static inline uint4 rotate_left(uint4 x, int n);
+	static inline void FF(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+	static inline void GG(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+	static inline void HH(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
+	static inline void II(uint4 &a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac);
 };
 
+std::string md5(const std::string str);
 
-#endif /*__ENCRYPT_MD_FIVE__*/
+#endif
